c1931efca7cc2f45d20806e223c11bec00e8679d
赵思阳 <zhaosiyang@s-MacBook-puro.local>
2019-01-03 15:57:13 -0500

    add SimpleChange as second parameter in callback function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-export function OnChange<T>(observer: (value: T) => void) {
+export interface SimpleChange<T> {
+    firstChange: boolean;
+    previousValue: T;
+    currentValue: T;
+    isFirstChange: () => boolean;
+}
+
+export function OnChange<T = any>(callback: (value: T, simpleChange?: SimpleChange<T>) => void) {
     let _cachedValue: T;
+    let _isFirstChange = true;
     return (target: any, key: PropertyKey) => {
         Object.defineProperty(target, key, {
             set: function (value) {
+                const oldValue = _cachedValue;
                 _cachedValue = value;
-                observer.call(this, _cachedValue);
+                const simpleChange: SimpleChange<T> = {
+                    firstChange: _isFirstChange,
+                    previousValue: oldValue,
+                    currentValue: _cachedValue,
+                    isFirstChange: () => _isFirstChange,
+                };
+                _isFirstChange = false;
+                callback.call(this, _cachedValue, simpleChange);
             },
             get: function () {
                 return _cachedValue;
