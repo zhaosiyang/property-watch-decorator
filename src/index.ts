8214278d162c9e46f3cd481cad900f3801bfc11c
Mr Wednesday <dev.yashu58@hotmail.com>
2019-06-14 11:13:53 +0400

    Added exception handling for the function when string is invalid

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,54 @@
 export interface SimpleChange<T> {
-    firstChange: boolean;
-    previousValue: T;
-    currentValue: T;
-    isFirstChange: () => boolean;
+  firstChange: boolean;
+  previousValue: T;
+  currentValue: T;
+  isFirstChange: () => boolean;
 }
 
 export function OnChange<T = any>(callback: ((value: T, simpleChange?: SimpleChange<T>) => void) | string) {
-    const cachedValueKey = Symbol();
-    const isFirstChangeKey = Symbol();
+  const cachedValueKey = Symbol();
+  const isFirstChangeKey = Symbol();
 
-    const isFunction = (fn: any) => fn.constructor === Function;
+  const isFunction = (fn) => fn.constructor === Function;
 
-    return (target: any, key: PropertyKey) => {
-        Object.defineProperty(target, key, {
-            set: function (value) {
+  const isValidFunction = (fn: Function, fnName: string): Function => {
+    if (!fn) {
+      throw new Error(`${fnName} is not a valid function`);
+    }
+    return fn;
+  };
 
-                const callBackFn = (isFunction(callback) ? callback : target[callback as string]) as Function;
+  return (target: any, key: PropertyKey) => {
+      
+    const callBackFn = (
+      isFunction(callback) ?
+        callback :
+        isValidFunction(target[callback as string], callback as string)
+    ) as Function;
 
-                // change status of "isFirstChange"
-                this[isFirstChangeKey] = this[isFirstChangeKey] === undefined;
-                
-                // No operation if new value is same as old value
-                if (!this[isFirstChangeKey] && this[cachedValueKey] === value) {
-                    return;
-                }
-                const oldValue = this[cachedValueKey];
-                this[cachedValueKey] = value;
-                const simpleChange: SimpleChange<T> = {
-                    firstChange: this[isFirstChangeKey],
-                    previousValue: oldValue,
-                    currentValue: this[cachedValueKey],
-                    isFirstChange: () => this[isFirstChangeKey],
-                };
-                callBackFn.call(this, this[cachedValueKey], simpleChange);
-            },
-            get: function () {
-                return this[cachedValueKey];
-            },
-        });
-    };
+    Object.defineProperty(target, key, {
+      set: function (value) {
+        // change status of "isFirstChange"
+        this[isFirstChangeKey] = this[isFirstChangeKey] === undefined;
+        // No operation if new value is same as old value
+
+        if (!this[isFirstChangeKey] && this[cachedValueKey] === value) {
+          return;
+        }
+
+        const oldValue = this[cachedValueKey];
+        this[cachedValueKey] = value;
+        const simpleChange: SimpleChange<T> = {
+          firstChange: this[isFirstChangeKey],
+          previousValue: oldValue,
+          currentValue: this[cachedValueKey],
+          isFirstChange: () => this[isFirstChangeKey],
+        };
+        callBackFn.call(this, this[cachedValueKey], simpleChange);
+      },
+      get: function () {
+        return this[cachedValueKey];
+      },
+    });
+  };
 }
